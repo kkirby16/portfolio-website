986831aee3a1b0cec1300bef28f639bab36607a5
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-09 03:26:33 -0800

    render navbar and 5 other components & add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import Introduction from "./components/introduction/Introduction";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Blogs from "./components/blogs/Blogs";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <div className="segments">
+        <Introduction />
+        <About />
+        <Projects />
+        <Blogs />
+        <Contact />
+      </div>
+
+      {/* intro, about, projects, blogs, contact  */}
     </div>
   );
 }
