9719168cab8c9ff39a1551b2808547f117c707f9
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-25 05:17:33 -0800

    change structure of jsx, text, & bring in a png in a different way

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -3,15 +3,19 @@ import "./introduction.scss";
 export default function Introduction() {
   return (
     <div className="introduction" id="intro">
-      <div className="greeting">Hi. I'm Kevin Kirby.</div>
-      <div className="role">Full-Stack Web Developer.</div>
-      <div className="circle1"></div>
-      <div className="circle2"></div>
-      <div className="triangle-topleft"></div>
-      <img
-        className="design"
-        src="C:/Users/kevinkirby/Downloads/taxi-715.png"
-      ></img>
+      <div className="greeting">Hi, I'm Kevin Kirby.</div>
+      <div className="role">
+        Full-Stack Web Developer with a&nbsp;
+        <i className="frontendWord">frontend</i>&nbsp;focus.
+      </div>
+      <div className="shapeContainer">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="triangle-topleft"></div>
+      </div>
+      <div className="designsContainer">
+        <img className="design" src="assets/taxi-715.png"></img>
+      </div>
     </div>
   );
 }
