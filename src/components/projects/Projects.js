e973718f44148100d7facbeb06d7cc80b9be6642
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-25 05:25:16 -0800

    add jsx structure for projects component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,14 @@
 import "./projects.scss";
 
 export default function Projects() {
-  return <div className="projects" id="projects"></div>;
+  return (
+    <div className="projects" id="projects">
+      <div className="projectsHeader">Projects</div>
+      <div className="projectContainer">
+        <div className="projectDiv">{/* <img></img> */}</div>
+        <div className="projectDiv"></div>
+        <div className="projectDiv"></div>
+      </div>
+    </div>
+  );
 }
