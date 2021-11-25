d72901e2e643ebacb3c8b019fcf7e9bce3644633
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-25 05:19:23 -0800

    change structure of logo part of navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,11 +4,10 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div className="outer-div">
-        <div className="logo">
-          <a className="logo-link" href="#intro">
-            KK
-          </a>
-        </div>
+        <a className="logo-link" href="#intro">
+          KK
+        </a>
+
         <div className="right-nav">
           <a className="right-nav-link" href="#about">
             About
