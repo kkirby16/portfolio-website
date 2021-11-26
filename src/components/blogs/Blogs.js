7c8c81dc8cb2f78e1129b366f8f785c27246ecc4
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-25 23:00:08 -0800

    start writing renderBlogs function & call it in return of component

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -1,5 +1,27 @@
 import "./blogs.scss";
 
-export default function Blogs() {
-  return <div className="blogs" id="blogs"></div>;
-}
+const Blogs = (props) => {
+  console.log("What are props here?:", props.blogs);
+
+  const renderBlogs = () => {
+    return props.blogs.map((blog) => {
+      return (
+        <div key={blog.id} className="blog">
+          <a href={blog.url} className="title">
+            {blog.title}
+            {console.log(blog.title)}
+          </a>
+          <img src={blog.social_image} alt={blog.title}></img>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="blogs" id="blogs">
+      {renderBlogs()}
+    </div>
+  );
+};
+
+export default Blogs;
