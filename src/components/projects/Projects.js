be110e5a1388d55705c331ff79f21a3df794fdc4
kkirby16 <kkirby16@my.whitworth.edu>
2021-11-28 23:16:16 -0800

    put some jsx structure in Projects component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,11 +3,20 @@ import "./projects.scss";
 export default function Projects() {
   return (
     <div className="projects" id="projects">
-      <div className="projectsHeader">Projects</div>
-      <div className="projectContainer">
-        <div className="projectDiv">{/* <img></img> */}</div>
-        <div className="projectDiv"></div>
-        <div className="projectDiv"></div>
+      <h1 className="projectsHeader">Projects</h1>
+      <div className="projectsContainer">
+        <div className="project">
+          <img src="assets/photo-sharer.png" alt="" className="photos" />
+          <h3>Photo Sharer</h3>
+        </div>
+        <div className="project">
+          <img src="assets/photo-sharer.png" alt="" className="photos" />
+          <h3>Photo Sharer</h3>
+        </div>
+        <div className="project">
+          <img src="assets/photo-sharer.png" alt="" className="photos" />
+          <h3>Photo Sharer</h3>
+        </div>
       </div>
     </div>
   );
