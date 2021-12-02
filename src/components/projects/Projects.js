511894081396c5ead1ae76fc5402398d358d68ec
kkirby16 <kkirby16@my.whitworth.edu>
2021-12-01 19:32:33 -0800

    change structure of html and add text

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,16 +6,19 @@ export default function Projects() {
       <h1 className="projectsHeader">Projects</h1>
       <div className="projectsContainer">
         <div className="project">
+          <h2>Photo Sharer</h2>
           <img src="assets/photo-sharer.png" alt="" className="photos" />
-          <h3>Photo Sharer</h3>
+          <h3>React / Redux / Rails / Thunk / Active Storage / Custom CSS</h3>
         </div>
         <div className="project">
+          <h2>Song Sharer</h2>
           <img src="assets/photo-sharer.png" alt="" className="photos" />
-          <h3>Photo Sharer</h3>
+          <h3>JavaScript / Rails API backend / AJAX / OOJS / Custom CSS</h3>
         </div>
         <div className="project">
-          <img src="assets/photo-sharer.png" alt="" className="photos" />
-          <h3>Photo Sharer</h3>
+          <h2>Nail the Trail</h2>
+          <img src="assets/nail-the-trail.png" alt="" className="photos" />
+          <h3>Rails / Custom CSS / Omniauth login and normal login</h3>
         </div>
       </div>
     </div>
