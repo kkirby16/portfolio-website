9309fae6cffb2ce1d713df95d1d741b9dd26b6ca
kkirby16 <kkirby16@my.whitworth.edu>
2021-12-03 02:41:33 -0800

    add h4 tag and add span tags around certain text elements

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -7,18 +7,30 @@ export default function Projects() {
       <div className="projectsContainer">
         <div className="project">
           <h2>Photo Sharer</h2>
-          <img src="assets/photo-sharer.png" alt="" className="photos" />
-          <h3>React / Redux / Rails / Thunk / Active Storage / Custom CSS</h3>
+          <div className="imageContainer">
+            <img src="assets/photo-sharer.png" alt="" className="photos" />
+            <h4>Description about the project</h4>
+          </div>
+          <h3>
+            React <span>/</span> Redux <span>/</span> Rails <span>/</span> Thunk{" "}
+            <span>/</span> Active Storage <span>/</span> Custom CSS
+          </h3>
         </div>
         <div className="project">
           <h2>Song Sharer</h2>
           <img src="assets/photo-sharer.png" alt="" className="photos" />
-          <h3>JavaScript / Rails API backend / AJAX / OOJS / Custom CSS</h3>
+          <h3>
+            JavaScript <span>/</span> Rails API backend <span>/</span> AJAX{" "}
+            <span>/</span> OOJS <span>/</span> Custom CSS
+          </h3>
         </div>
         <div className="project">
           <h2>Nail the Trail</h2>
           <img src="assets/nail-the-trail.png" alt="" className="photos" />
-          <h3>Rails / Custom CSS / Omniauth login and normal login</h3>
+          <h3>
+            Rails <span>/</span> Custom CSS <span>/</span> Omniauth login and
+            normal login
+          </h3>
         </div>
       </div>
     </div>
