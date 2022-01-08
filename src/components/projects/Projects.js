fb414eb1e967cdbb77eabd43929d2370756c4e57
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-07 18:12:52 -0800

    add html structure to make sure html structure is same for each project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -16,7 +16,11 @@ export default function Projects() {
               target="_blank"
               class="project-info-flex-item"
             >
-              <GitHubIcon style={{ color: "black" }} fontSize="medium" />
+              <GitHubIcon
+                style={{ color: "black" }}
+                fontSize="medium"
+                className="githubIcon"
+              />
             </a>
             <h3 class="project-info-flex-item live-site-text">Live</h3>{" "}
             <a href="https://youtu.be/cJb1p153Y-0" target="_blank">
@@ -31,40 +35,73 @@ export default function Projects() {
             <h4>Description about the project</h4>
           </div>
           <h3>
-            React <span>/</span> Redux <span>/</span> Rails <span>/</span> Thunk{" "}
-            <span>/</span> Active Storage <span>/</span> Custom CSS
+            React <span>/</span> Redux <span>/</span> Rails <span>/</span>{" "}
+            PostgreSQL <span>/</span> Active Storage <span>/</span> Custom CSS
           </h3>
         </div>
         <div className="project">
-          <h2>
-            Song Sharer{" "}
+          <div className="project-info">
+            <h2 class="project-info-flex-item">Song Sharer </h2>
             <a
               href="https://github.com/kkirby16/song_sharer_frontend"
               target="_blank"
+              class="project-info-flex-item"
             >
-              <GitHubIcon style={{ color: "black" }} fontSize="medium" />
+              <GitHubIcon
+                style={{ color: "black" }}
+                fontSize="medium"
+                className="githubIcon"
+              />
             </a>
-          </h2>
-          <img src="assets/photo-sharer.png" alt="" className="photos" />
+            {/* <h3 class="project-info-flex-item live-site-text">Live</h3>{" "} */}
+            <a href="https://youtu.be/WlAL8AUhsrk" target="_blank">
+              <YouTubeIcon
+                style={{ color: "black" }}
+                className="youtubeIcon project-info-flex-item"
+              />
+            </a>
+          </div>
+          <div className="imageContainer">
+            <img src="assets/photo-sharer.png" alt="" className="photos" />
+            <h4>Description about the project</h4>
+          </div>
           <h3>
-            JavaScript <span>/</span> Rails API backend <span>/</span> AJAX{" "}
-            <span>/</span> OOJS <span>/</span> Custom CSS
+            JavaScript <span>/</span> Rails API backend <span>/</span>{" "}
+            PostgreSQL <span>/</span> AJAX <span>/</span> Custom CSS
           </h3>
         </div>
         <div className="project">
-          <h2>
-            Nail the Trail{" "}
+          <div className="project-info">
+            <h2 class="project-info-flex-item">Nail the Trail </h2>
             <a
               href="https://github.com/kkirby16/Nail-The-Trail"
               target="_blank"
+              class="project-info-flex-item"
             >
-              <GitHubIcon style={{ color: "black" }} fontSize="medium" />
+              <GitHubIcon
+                style={{ color: "black" }}
+                fontSize="medium"
+                className="githubIcon"
+              />
             </a>
-          </h2>
-          <img src="assets/nail-the-trail.png" alt="" className="photos" />
+            {/* <h3 class="project-info-flex-item live-site-text">Live</h3>{" "} */}
+            <a
+              href="https://www.youtube.com/watch?v=2kKjG923AMg"
+              target="_blank"
+            >
+              <YouTubeIcon
+                style={{ color: "black" }}
+                className="youtubeIcon project-info-flex-item"
+              />
+            </a>
+          </div>
+          <div className="imageContainer">
+            <img src="assets/nail-the-trail.png" alt="" className="photos" />
+            <h4>Description about the project</h4>
+          </div>
           <h3>
-            Rails <span>/</span> Custom CSS <span>/</span> Omniauth login and
-            normal login
+            Rails <span>/</span> SQLite <span>/</span> Custom CSS <span>/</span>{" "}
+            Omniauth Login and normal login
           </h3>
         </div>
       </div>
