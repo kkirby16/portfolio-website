bf14e236ece3e07075a83e3f1ffef49cf27f23d5
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-08 00:20:35 -0800

    add basic jsx structure for about header, about description, and skills

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,7 +3,19 @@ import "./about.scss";
 export default function About() {
   return (
     <div className="about" id="about">
-      <div>Hi there</div>
+      <h2 className="aboutHeader">About</h2>
+      <div className="about-description">
+        Full-stack software developer with a background in sales and SaaS sales
+        development. An adept analyst and communicator who continually aims to
+        improve the user experience. Bringing 2+ years of SaaS sales development
+        experience and a deep understanding of the software development life
+        cycle, possesses ability to interface with external customers and
+        prospects as well as internally with technical and non-technical
+        stake-holders. Seeking frontend or full-stack development roles that
+        focus on delivering valuable experiences to users, customers and
+        stake-holders.
+      </div>
+      <div className="skills"></div>
     </div>
   );
 }
