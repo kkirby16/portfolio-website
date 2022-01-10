44849900c6f481b9a49b4613c2060d522e6e53ac
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-09 22:03:17 -0800

    import useState & a method from emailjs-com, add basic onSubmit handling functionality, and start changing form so I can get it to send an email to me

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,28 +2,39 @@ import "./contact.scss";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { Icon } from "@iconify/react";
+import { useState } from "react";
+import { send } from "emailjs-com";
 
 export default function Contact() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="contact" id="contact">
       <section class="get-in-touch">
         <h1 class="title">Contact me</h1>
-        <form class="contact-form row">
+        <form class="contact-form row" onSubmit={handleSubmit}>
           <div class="form-field col x-50">
             <input
               id="name"
+              name="from_name"
               class="input-text js-input"
               type="text"
               placeholder="Name"
+              value={toSend.from_name}
+              onChange={handleChange}
               required
             />
           </div>
           <div class="form-field col x-50">
             <input
               id="email"
+              name="from_email"
               class="input-text js-input"
               type="email"
               placeholder="Email"
+              value={toSend.from_email}
+              onChange={handleChange}
               required
             />
           </div>
