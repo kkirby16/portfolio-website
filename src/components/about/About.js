b43283ebb17db2880de3062110fe054e9ce53da5
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-10 22:53:39 -0800

    add certain floating classes to skill icons & change size of a skill icon

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -24,50 +24,50 @@ export default function About() {
         <h2>Skills</h2>
         <ul>
           <li>
-            <Icon icon="bx:bxl-react" width="47" height="47" />{" "}
+            <Icon icon="bx:bxl-react" width="47" height="47" className="floating1" />{" "}
           </li>
           <li>
-            <Icon icon="bx:bxl-redux" width="46" height="46" />
+            <Icon icon="bx:bxl-redux" width="46" height="46" className="floating2" />
           </li>
           <li>
-            <Icon icon="cib:javascript" width="43" height="43" />
+            <Icon icon="cib:javascript" width="42" height="42" className="floating3"/>
           </li>
           <li>
-            <Icon icon="fa-brands:css3-alt" width="43" height="43" />
+            <Icon icon="fa-brands:css3-alt" width="43" height="43" className="floating1"/>
           </li>
           <li>
-            <Icon icon="cib:sass-alt" width="44" height="44" />
+            <Icon icon="cib:sass-alt" width="44" height="44" className="floating2"/>
           </li>
           <li>
-            <Icon icon="cib:html5-shield" width="43" height="43" />
+            <Icon icon="cib:html5-shield" width="43" height="43" className="floating3"/>
           </li>
           <li>
-            <Icon icon="cib:ruby" width="42" height="42" />
+            <Icon icon="cib:ruby" width="42" height="42" className="floating1"/>
           </li>
           <li>
-            <Icon icon="simple-icons:rubyonrails" width="59.5" height="59.5" />
+            <Icon icon="simple-icons:rubyonrails" width="59.5" height="59.5" className="floating2"/>
           </li>
           <li>
-            <Icon icon="eos-icons:api" color="black" width="47" height="47" />
+            <Icon icon="eos-icons:api" color="black" width="47" height="47" className="floating3"/>
           </li>
           <li>
-            <Icon icon="cib:postgresql" width="43" height="43" />
+            <Icon icon="cib:postgresql" width="43" height="43" className="floating1"/>
           </li>
           <li>
-            <Icon icon="simple-icons:sqlite" width="43" height="43" />
+            <Icon icon="simple-icons:sqlite" width="43" height="43" className="floating2"/>
           </li>
           <li>
-            <Icon icon="carbon:sql" color="black" width="45" height="45" />
+            <Icon icon="carbon:sql" color="black" width="45" height="45" className="floating3"/>
           </li>
 
           <li>
-            <Icon icon="file-icons:sinatra" width="44" height="44" />
+            <Icon icon="file-icons:sinatra" width="44" height="44" className="floating1"/>
           </li>
           <li>
-            <Icon icon="cib:heroku" color="black" width="43" height="43" />
+            <Icon icon="cib:heroku" color="black" width="43" height="43" className="floating2"/>
           </li>
           <li>
-            <Icon icon="file-icons:netlify" width="43" height="43" />
+            <Icon icon="file-icons:netlify" width="43" height="43" className="floating3"/>
           </li>
           <li>
             <Icon
@@ -75,13 +75,14 @@ export default function About() {
               color="black"
               width="43"
               height="43"
+              className="floating1"
             />
           </li>
           <li>
-            <Icon icon="cib:git" width="43" height="43" />
+            <Icon icon="cib:git" width="43" height="43" className="floating2"/>
           </li>
           <li>
-            <Icon icon="el:wordpress" width="43" height="43" />
+            <Icon icon="el:wordpress" width="43" height="43" className="floating3"/>
           </li>
           <li>
             <Icon
@@ -89,7 +90,12 @@ export default function About() {
               color="black"
               width="43"
               height="43"
+              className="floating1"
             />
+          </li>
+          <li>
+          <Icon icon="akar-icons:github-fill" color="black" width="43" height="43" className="floating2" />
+
           </li>
         </ul>
       </div>
