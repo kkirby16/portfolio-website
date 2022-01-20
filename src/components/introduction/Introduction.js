520a2d32a7c82ae46b800890cba6edb40ca3bb17
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-19 18:41:55 -0800

    add text for testing github continuous deployment

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -3,28 +3,27 @@ import "./introduction.scss";
 export default function Introduction() {
   return (
     <div className="introduction" id="intro">
-      <div className="greeting">Hi, I'm Kevin Kirby.</div>
+      <div className="greeting">Hi, I'm Kevin Kirby. (test)</div>
       <div className="role">
         Full-Stack Web Developer with a&nbsp;
         <i className="frontendWord">frontend</i>&nbsp;focus.
       </div>
       <div className="shapeContainer">
-         <div className="circle1"></div>
-         <div className="circle2"></div>
-         <div className="circle3"></div>
-         <div className="circle4"></div> 
-         <div className="circle5"></div> 
-         <div className="circle6"></div>
-         <div className="circle7"></div>
-         <div className="circle8"></div>
-         <div className="circle9"></div>
-         <div className="circle10"></div>
-         <div className="circle11"></div>
-         <div className="circle12"></div>
-         <div className="circle13"></div>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <div className="circle3"></div>
+        <div className="circle4"></div>
+        <div className="circle5"></div>
+        <div className="circle6"></div>
+        <div className="circle7"></div>
+        <div className="circle8"></div>
+        <div className="circle9"></div>
+        <div className="circle10"></div>
+        <div className="circle11"></div>
+        <div className="circle12"></div>
+        <div className="circle13"></div>
 
-
-         <div className="triangle-topleft"></div>
+        <div className="triangle-topleft"></div>
       </div>
       <div className="designsContainer">
         <img className="design" src="assets/taxi-715.png"></img>
