a4bc3dc4c3f95d7355d81d610fe7ee89604adc21
kkirby16 <kkirby16@my.whitworth.edu>
2022-01-20 21:34:51 -0800

    add outer div around several circle divs & change diagonal arrow image src and className

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -10,25 +10,27 @@ export default function Introduction() {
       </div>
       <div className="shapeContainer">
         <div className="circle1"></div>
-        <div className="circle2"></div>
-        <div className="circle3"></div>
-        <div className="circle4"></div>
-        <div className="circle5"></div>
-        <div className="circle6"></div>
-        <div className="circle7"></div>
-        <div className="circle8"></div>
-        <div className="circle9"></div>
-        <div className="circle10"></div>
-        <div className="circle11"></div>
-        <div className="circle12"></div>
-        <div className="circle13"></div>
+        <div className="circleGridDiv">
+          <div className="circle2"></div>
+          <div className="circle3"></div>
+          <div className="circle4"></div>
+          <div className="circle5"></div>
+          <div className="circle6"></div>
+          <div className="circle7"></div>
+          <div className="circle8"></div>
+          <div className="circle9"></div>
+          <div className="circle10"></div>
+          <div className="circle11"></div>
+          <div className="circle12"></div>
+          <div className="circle13"></div>
+        </div>
 
         <div className="triangle-topleft"></div>
       </div>
       <div className="designsContainer">
         <img className="design" src="taxi-715.png"></img>
       </div>
-      <img src="icons8-diagonal-arrow-64.png" className="diagonalArrow"></img>
+      <img src="diagonal-arrow-design.png" className="diagonalArrowDesign" />
     </div>
   );
 }
